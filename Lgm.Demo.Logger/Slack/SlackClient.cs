9ab50d5437a5c6cb7801a81dcdb2fe9e15877677
Lenin Girón <lenin@dev.pagaloop.com>
2021-04-30 21:00:22 -0500

    se agrega el cliente de slack

---
+++ Lgm.Demo.Logger/Slack/SlackClient.cs
@@ -0,0 +1,14 @@
+﻿namespace Lgm.Demo.Logger.Slack
+{
+    public class SlackClient : ISlackClient
+    {
+        public string PostMessage(string text, string channel = null)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public interface ISlackClient
+    {
+        string PostMessage(string text, string channel = null);
+    }
+}
